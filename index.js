06408b42ec0e0f109e473876ad3a68d6d8e783e8
Edward <edpantzar@gmail.com>
2018-05-17 14:02:31 -0500

    Added event listener to toggle light class

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 (function() {
   'use strict';
+  const stopLight = document.getElementById("stopLight");
+  const slowLight = document.getElementById("slowLight");
+  const goLight = document.getElementById("goLight");
+  const stopButton = document.getElementById("stopButton");
+  const slowButton = document.getElementById("slowButton");
+  const goButton = document.getElementById("goButton");
 
+  document.addEventListener('DOMContentLoaded', function(){
+    stopButton.addEventListener("click", function(event){
+      event.preventDefault();
+      if (stopLight.classList.contains("bulb")){
+        stopLight.classList.toggle("stop");
+      }
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function(){
+    slowButton.addEventListener("click", function(event){
+      event.preventDefault();
+      if (slowLight.classList.contains("bulb")){
+        slowLight.classList.toggle("slow");
+      }
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function(){
+    goButton.addEventListener("click", function(event){
+      event.preventDefault();
+      if (goLight.classList.contains("bulb")){
+        goLight.classList.toggle("go");
+      }
+    });
+  });
   // YOUR CODE HERE
 })();
